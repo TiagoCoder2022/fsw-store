22ab8ae390a60c9d33cfbcea4cdf4b133e26ca97
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-10-22 17:55:57 -0300

    feat: add page to show actegory products

---
+++ src/constants/category-icon.tsx
@@ -0,0 +1,17 @@
+import {
+  KeyboardIcon,
+  MonitorIcon,
+  HeadphonesIcon,
+  SquareIcon,
+  SpeakerIcon,
+  MouseIcon,
+} from "lucide-react";
+
+export const CATEGORY_ICON = {
+  keyboards: <KeyboardIcon size={17} />,
+  monitors: <MonitorIcon size={17} />,
+  headphones: <HeadphonesIcon size={17} />,
+  mousepads: <SquareIcon size={17} />,
+  speakers: <SpeakerIcon size={17} />,
+  mouses: <MouseIcon size={17} />,
+};
