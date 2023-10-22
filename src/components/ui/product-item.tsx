06b5285918af504cdd897e76d4450ee24b99dced
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-10-21 21:36:55 -0300

    fix: ensure correct product total price is calculated

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -37,7 +37,9 @@ const ProductItem = ({ product }: ProductItemProps) => {
         <div className="flex items-center gap-2">
           {product.discountPercentage > 0 ? (
             <>
-              <p className="font-bold">R$ {product.totalPrice.toFixed(2)}</p>
+              <p className="font-semibold">
+                R$ {product.totalPrice.toFixed(2)}
+              </p>
 
               <p className="text-xs line-through opacity-75">
                 R$ {Number(product.basePrice).toFixed(2)}
