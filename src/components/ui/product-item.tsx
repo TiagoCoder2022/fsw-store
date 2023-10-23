bced3c16333f26d233c05c4cbbfac93dcd5e3afb
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-10-22 23:23:19 -0300

    feat: ensure user goes to product datails page on product click

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -2,58 +2,61 @@ import { ProductWithTotalPrice } from "@/helpers/product";
 import { ArrowDownIcon } from "lucide-react";
 import Image from "next/image";
 import { Badge } from "./badge";
+import Link from "next/link";
 
 interface ProductItemProps {
   product: ProductWithTotalPrice;
 }
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex flex-col gap-4">
-      <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
-        <Image
-          src={product.imageUrls[0]}
-          height={0}
-          width={0}
-          sizes="100vw"
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
-          style={{
-            objectFit: "contain",
-          }}
-          alt={product.name}
-        />
+    <Link href={`/product/${product.slug}`}>
+      <div className="flex flex-col gap-4">
+        <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
+          <Image
+            src={product.imageUrls[0]}
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+            style={{
+              objectFit: "contain",
+            }}
+            alt={product.name}
+          />
 
-        {product.discountPercentage > 0 && (
-          <Badge className="absolute left-3 top-3">
-            <ArrowDownIcon size={14} /> {product.discountPercentage}%
-          </Badge>
-        )}
-      </div>
-
-      <div className="flex flex-col gap-1">
-        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-          {product.name}
-        </p>
-
-        <div className="flex items-center gap-2">
-          {product.discountPercentage > 0 ? (
-            <>
-              <p className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
-                R$ {product.totalPrice.toFixed(2)}
-              </p>
-
-              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
-                R$ {Number(product.basePrice).toFixed(2)}
-              </p>
-            </>
-          ) : (
-            <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
-              R$ {product.basePrice.toFixed(2)}
-            </p>
+          {product.discountPercentage > 0 && (
+            <Badge className="absolute left-3 top-3">
+              <ArrowDownIcon size={14} /> {product.discountPercentage}%
+            </Badge>
           )}
-          <p>{}</p>
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+            {product.name}
+          </p>
+
+          <div className="flex items-center gap-2">
+            {product.discountPercentage > 0 ? (
+              <>
+                <p className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
+                  R$ {product.totalPrice.toFixed(2)}
+                </p>
+
+                <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
+                  R$ {Number(product.basePrice).toFixed(2)}
+                </p>
+              </>
+            ) : (
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
+                R$ {product.basePrice.toFixed(2)}
+              </p>
+            )}
+            <p>{}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
