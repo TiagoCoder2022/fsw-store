74828b633bddc4b18f1039950f454a452b2bd35b
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-10-23 12:24:23 -0300

    feat: add Discoutbadge component

--- src/app/product/[slug]/components/product-info.tsx
+++ src/app/product/[slug]/components/product-info.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import DiscountBadge from "@/components/ui/discount-badge";
 import { ProductWithTotalPrice } from "@/helpers/product";
 import {
   ArrowDown,
@@ -36,10 +36,7 @@ const ProductInfo = ({
       <div className="flex items-center gap-2">
         <h1 className="text-xl font-bold">R$ {totalPrice.toFixed(2)}</h1>
         {discountPercentage > 0 && (
-          <Badge className=" px-2 py-[2px]">
-            <ArrowDown size={14} />
-            {discountPercentage}%
-          </Badge>
+          <DiscountBadge>{discountPercentage}%</DiscountBadge>
         )}
       </div>
       {discountPercentage > 0 && (
