21bf938b2f50f1b008edea2ae8fee7f88ffaf3ac
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-10-27 13:28:52 -0300

    feat: add loading component

---
+++ src/app/product/[slug]/loading.tsx
@@ -0,0 +1,11 @@
+import { LoaderIcon } from "lucide-react";
+
+const Loading = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center ">
+      <LoaderIcon size={27} />
+    </div>
+  );
+};
+
+export default Loading;
