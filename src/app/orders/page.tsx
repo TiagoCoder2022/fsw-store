3bc9ce7baf84c565ef4c499610a89ca247e6d019
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-10-29 20:55:08 -0300

    feat: add OrderProductItem component

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -17,14 +17,18 @@ async function OrderPage() {
       userId: (user as any).id,
     },
     include: {
-      orderProducts: true,
+      orderProducts: {
+        include: {
+          product: true,
+        },
+      },
     },
   });
 
   return (
     <div className="p-5">
       <Badge
-        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        className="mb-5 w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
       >
         <PackageSearchIcon size={16} />
