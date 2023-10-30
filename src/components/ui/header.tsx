15c8fb7ff0bc3ef3b5e993fc76c3ab11f4f81d5e
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-10-30 02:05:51 -0300

    feat: show in the cart icon the products items quantity

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -24,9 +24,16 @@ import { AvatarFallback } from "@radix-ui/react-avatar";
 import { Separator } from "@radix-ui/react-separator";
 import Link from "next/link";
 import Cart from "./cart";
+import { useContext } from "react";
+import { CartContext } from "@/providers/cart";
 
 const Header = () => {
   const { status, data } = useSession();
+
+  const { products } = useContext(CartContext);
+
+  const cartQuantityItems = products.length;
+
   const handleLoginClick = async () => {
     await signIn();
   };
@@ -148,7 +155,12 @@ const Header = () => {
 
       <Sheet>
         <SheetTrigger>
-          <Button size="icon" variant="outline">
+          <Button size="icon" variant="outline" className="relative">
+            {cartQuantityItems > 0 && (
+              <span className="absolute right-[calc(-1.25rem/2)] top-[calc(-1.25rem/2)] flex h-6 w-6 items-center justify-center rounded-lg bg-primary text-sm font-bold">
+                {cartQuantityItems}
+              </span>
+            )}
             <ShoppingCartIcon />
           </Button>
         </SheetTrigger>
