41d52c5c9e06bd6e0a54aca3d0eb1bddd4d6e473
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-11-11 23:47:55 -0300

    fix: ensure orders are shown only for authenticated user

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -8,15 +8,20 @@ import OrderItem from "./components/order-item";
 export const dynamic = "force-dynamic";
 
 async function OrderPage() {
-  const user = getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-  if (!user) {
-    return <p>Access Denied</p>;
+  if (!session || !session.user) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center gap-2 p-5">
+        <h2 className="font-bold">Acesso Negado!</h2>
+        <p className="text-sm opacity-60">Faça login para ver seus pedidos</p>
+      </div>
+    );
   }
 
   const orders = await prismaClient.order.findMany({
     where: {
-      userId: (user as any).id,
+      userId: session.user.id,
     },
     include: {
       orderProducts: {
@@ -28,7 +33,7 @@ async function OrderPage() {
   });
 
   return (
-    <div className="p-5">
+    <div className="p-5 lg:container lg:mx-auto lg:py-10">
       <Badge
         className=" w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
