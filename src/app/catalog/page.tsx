54ad89577cebc12fd9236b1e0164565dfaf243c4
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-11-12 21:20:32 -0300

    style: add desktop responsivity to catalog page

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -6,7 +6,7 @@ import CategoryItem from "./components/category-item";
 const CatalogPage = async () => {
   const categories = await prismaClient.category.findMany({});
   return (
-    <div className=" flex flex-col gap-8 p-5 lg:px-32">
+    <div className="container mx-auto p-5">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -15,7 +15,7 @@ const CatalogPage = async () => {
         Catálogo
       </Badge>
 
-      <div className="grid grid-cols-2 gap-8">
+      <div className="grid grid-cols-2 gap-8 pt-5">
         {categories.map((category) => (
           <CategoryItem key={category.id} category={category} />
         ))}
