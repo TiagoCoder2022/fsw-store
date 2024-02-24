418d0746be5d09ff3399ca29c841f1b0d64ad629
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-02-24 01:54:39 -0300

    feat: ensure products are fetched on product page

--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -1,4 +1,7 @@
-const ProductsPage = () => {
+import { prismaClient } from "@/lib/prisma";
+
+const ProductsPage = async () => {
+  const products = await prismaClient.product.findMany();
   return <h1>Products</h1>;
 };
 
