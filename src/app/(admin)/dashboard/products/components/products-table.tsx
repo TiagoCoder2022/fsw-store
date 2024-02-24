a31fc474798850884a2619fc4cda470aaac30c72
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-02-24 14:20:05 -0300

    feat: add product tables

---
+++ src/app/(admin)/dashboard/products/components/products-table.tsx
@@ -0,0 +1,53 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { ProductWithTotalPrice } from "@/helpers/product";
+
+export type ProductWithTotalPriceAndCategory = ProductWithTotalPrice & {
+  category: {
+    name: string;
+  };
+};
+
+interface ProductsTableProps {
+  products: ProductWithTotalPriceAndCategory[];
+}
+
+const ProductsTable = ({ products }: ProductsTableProps) => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Nome</TableHead>
+          <TableHead>Categoria</TableHead>
+          <TableHead>Preço total</TableHead>
+          <TableHead>Preço base</TableHead>
+          <TableHead>Vendidos</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {products.map((product) => (
+          <TableRow key={product.id}>
+            <TableCell>{product.name}</TableCell>
+
+            <TableCell>{(product as any).category.name}</TableCell>
+
+            <TableCell>R$ {product.totalPrice.toFixed(2)}</TableCell>
+
+            <TableCell>R$ {product.basePrice.toFixed(2)}</TableCell>
+
+            <TableCell>0</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default ProductsTable;
